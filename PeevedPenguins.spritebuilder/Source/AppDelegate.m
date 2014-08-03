bd3a8d922fe764795ec637d343cc33813dfc567c
Wolfgang Kandek <wkandek@100330MBP15.local>
2014-08-03 14:19:36 +0200

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/AppDelegate.m
+++ PeevedPenguins.spritebuilder/Source/AppDelegate.m
@@ -53,10 +53,7 @@
     //[cocos2dSetup setObject:kEAGLColorFormatRGB565 forKey:CCConfigPixelFormat];
     
     [self setupCocos2dWithOptions:cocos2dSetup];
-    
-    [CCBReader load:@"Penguin"];
-    [CCBReader load:@"Seal"];
-    
+ 
     return YES;
 }
 
