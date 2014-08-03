dc51b7afd3df527c030daf591de2db93521634a8
Wolfgang Kandek <wkandek@100330MBP15.local>
2014-08-03 14:56:02 +0200

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Wolfgang Kandek on 8/3/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
