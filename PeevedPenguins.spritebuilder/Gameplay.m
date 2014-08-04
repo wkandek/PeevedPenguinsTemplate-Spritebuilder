da44eb36b50eded3fe22a652412287ab50ad04a1
Wolfgang Kandek <wkandek@100330MBP15.local>
2014-08-04 08:36:44 +0200

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -31,6 +31,7 @@
     // visualize physics bodies & joints
     //_physicsNode.debugDraw = TRUE;
     // nothing shall collide with our invisible nodes
+    _levelNode.physicsBody.collisionMask = @[]
     _pullbackNode.physicsBody.collisionMask = @[];
     _mouseJointNode.physicsBody.collisionMask = @[];
     _physicsNode.collisionDelegate = self;
